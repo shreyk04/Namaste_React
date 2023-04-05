7cc3007ed31c3951bfd1a8f9057b4c2d8d505b67
“[firstname <kadshreya07@gmail.com>
2023-04-05 14:27:20 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,16 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React"
+);
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Heading1"),
+    React.createElement("h2", {}, "Heading2"),
+  ])
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
